0930c19e166b8d9fb4a33a6dcdf390c54d297a9d
Rafael Avila <a22300193@ceti.mx>
2026-02-20 22:45:21 -0600

    Correccion estilos

--- public/detalle_producto.php
+++ public/detalle_producto.php
@@ -4,9 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title id="pageTitle">Producto - Wooden House</title>
-  <link rel="stylesheet" href="./assets/css/variables.css">
-  <link rel="stylesheet" href="./assets/css/styles.css">
-  <link rel="stylesheet" href="./assets/css/detalle_producto.css">
+  <link rel="stylesheet" href="/assets/css/variables.css">
+  <link rel="stylesheet" href="/assets/css/styles.css">
+  <link rel="stylesheet" href="/assets/css/detalle_producto.css">
+  <script src="/assets/js/detalle_producto.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
